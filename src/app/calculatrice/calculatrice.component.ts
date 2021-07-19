a169e7002ccae73d37a2564868fbf00609e021fd
teddy <teddy.koudoro@gmail.com>
2021-07-19 20:05:14 +0200

    maj function

--- src/app/calculatrice/calculatrice.component.ts
+++ src/app/calculatrice/calculatrice.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { element } from 'protractor';
+import { unescapeIdentifier } from '@angular/compiler';
 
 @Component({
   selector: 'app-calculatrice',
@@ -24,5 +25,57 @@ export class CalculatriceComponent implements OnInit {
   validate(): void {
     this.numbers.push(parseFloat(this.temporaryNumber));
     this.temporaryNumber = '';
+  }
+
+  operation(symbol: string): void {
+    console.log(symbol);
+    if (this.numbers.length < 2) {
+      return;
+    }
+
+    switch (symbol) {
+      case '+':
+        this.numbers[this.numbers.length - 2] =
+          this.numbers[this.numbers.length - 1] +
+          this.numbers[this.numbers.length - 2];
+        this.numbers.pop();
+        break;
+
+      case '-':
+        this.numbers[this.numbers.length - 2] =
+          this.numbers[this.numbers.length - 1] -
+          this.numbers[this.numbers.length - 2];
+        this.numbers.pop();
+        break;
+
+      case '%':
+        this.numbers[this.numbers.length - 2] =
+          this.numbers[this.numbers.length - 1] /
+          this.numbers[this.numbers.length - 2];
+        this.numbers.pop();
+        break;
+
+      case 'log':
+        this.numbers[this.numbers.length - 1] = Math.log(
+          this.numbers[this.numbers.length - 1]
+        );
+        break;
+
+      case 'x':
+        this.numbers[this.numbers.length - 2] =
+          this.numbers[this.numbers.length - 1] *
+          this.numbers[this.numbers.length - 2];
+        this.numbers.pop();
+        break;
+    }
+  }
+
+  reset(): void {
+    this.numbers = [];
+    this.temporaryNumber = '';
+  }
+
+  comma(): void {
+    this.temporaryNumber += '.';
   }
 }
