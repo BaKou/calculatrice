5e88df86cbf254e1ce2b85ea87a3fb23fb17af13
teddy <teddy.koudoro@gmail.com>
2021-07-21 07:34:43 +0200

    add test

--- src/app/calculatrice/calculatrice.component.spec.ts
+++ src/app/calculatrice/calculatrice.component.spec.ts
@@ -29,7 +29,7 @@ describe('CalculatriceComponent', () => {
     expect(component.temporaryNumber).toBe('1');
   });
 
-  it('should create', () => {
+  it('should ad number to temporaryNumber', () => {
     //GIVEN
 
     //WHEN
@@ -41,6 +41,114 @@ describe('CalculatriceComponent', () => {
   });
 
   it('should save number in array', () => {
-    expect(component).toBeTruthy();
+    //GIVEN
+    component.addNumber('1');
+    component.addNumber('2');
+    //WHEN
+    component.validate();
+
+    //THEN
+    expect(component.numbers.length).toBe(1);
+    expect(component.numbers[0]).toBe(12);
+  });
+
+  it('should add comma to number', () => {
+    //GIVEN
+    component.addNumber('1');
+
+    //WHEN
+    component.comma();
+
+    //THEN
+    expect(component.temporaryNumber).toBe('1.');
+  });
+
+  it('should reset array and temporaryNumber', () => {
+    //GIVEN
+    component.addNumber('1');
+    component.addNumber('2');
+    component.validate();
+    component.addNumber('3');
+
+    //WHEN
+    component.reset();
+
+    //THEN
+    expect(component.numbers.length).toBe(0);
+    expect(component.temporaryNumber).toBe('');
+  });
+
+  it('should erase one number', () => {
+    //GIVEN
+    component.addNumber('1');
+    component.addNumber('2');
+
+    //WHEN
+    component.deleteOne();
+
+    //THEN
+    expect(component.temporaryNumber).toBe('1');
+  });
+
+  describe('Operation', () => {
+    it('should add two number', () => {
+      //GIVEN
+      component.addNumber('1');
+      component.validate();
+      component.addNumber('2');
+      component.validate();
+
+      //WHEN
+      component.operation('+');
+
+      //THEN
+      expect(component.numbers[0]).toBe(3);
+      expect(component.numbers.length).toBe(1);
+    });
+
+    it('should soustrate two number', () => {
+      ///GIVEN
+      component.addNumber('1');
+      component.validate();
+      component.addNumber('2');
+      component.validate();
+
+      //WHEN
+      component.operation('-');
+
+      //THEN
+      expect(component.numbers[0]).toBe(1);
+      expect(component.numbers.length).toBe(1);
+    });
+
+    it('should divise two number', () => {
+      //GIVEN
+      component.addNumber('1');
+      component.validate();
+      component.addNumber('2');
+      component.validate();
+
+      //WHEN
+      component.operation('%');
+
+      //THEN
+      expect(component.numbers[0]).toBe(2);
+      expect(component.numbers.length).toBe(1);
+    });
+
+    it('should multiply two number', () => {
+      //GIVEN
+      component.addNumber('1');
+      component.validate();
+      component.addNumber('2');
+      component.validate();
+
+      //WHEN
+      component.operation('x');
+
+      //THEN
+      expect(component.numbers[0]).toBe(2);
+      expect(component.numbers.length).toBe(1);
+    });
   });
 });
